feadf24e34e8dac455658a555df8a4fa96edd23d
2118339 <2118339@LTIN368401.cts.com>
2024-03-31 18:47:52 +0530

    branch2 commit

--- src/module-info.java
+++ src/module-info.java
@@ -6,11 +6,11 @@
  *
  */
 module Branching {
-	 // Commit on master version
+	 
 	// commit 2
 	// commit 3
 	// commit 4 on git hub
 	// branch1 commit 1
 	// branch1 commit 2
-	// master commit
+	//branch2
 }
